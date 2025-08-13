be7daadecc5218e30e6612370d65300f753fe219
amitpal603-ops <ap036776@gmail.com>
2025-08-13 15:17:43 +0530

    change

---
+++ Interview.js
@@ -0,0 +1,26 @@
+const arr1 = []
+const arr2 = []
+
+ const res = arr1 === arr2 ? true: false
+
+//  console.log(res); output false
+
+const obj1 = {}
+const obj2 = {}
+
+const res1 = obj1 === obj2 ? true : false
+// console.log(res1); output false
+
+const obj = {}
+const arr = []
+
+const res2 = obj === arr ? true:false
+
+// console.log(res2); output false
+
+const arr3 = []
+const obj3 = {}
+
+const res3 = arr3 === obj3 ? true: false
+
+// console.log(res3);  output is false
--- arrayMethd.js
+++ arrayMethd.js
@@ -1,2 +1,15 @@
-let a = 9
-console.log(a);
+let arr = [1,2,3,4,5]
+
+// 1 push method
+
+const add = arr.push(9,[1,2,3])
+// console.log(add);
+// console.log(arr);
+
+// pop 
+
+const pop = arr.pop()
+// console.log(pop);
+// console.log(arr);
+
+
